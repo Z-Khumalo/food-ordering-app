42d4c7a4227c57e9ca94fa853cd37ac7821de6cb
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-05-11 12:03:26 +0200

    Updated admin login

--- Admin_Login.r
+++ Admin_Login.r
@@ -5,23 +5,38 @@ library(RPostgreSQL)
 source("con.R")
 
 
-# Prompt user for login credentials
-username <- readline("Enter your username: ")
-password <- readline("Enter your password: ")
 
-# Construct SQL query to check credentials against database
-sql <- paste0("SELECT * FROM Administrator WHERE Admin_user_name = '", username, "' AND Admin_password = '", password, "'")
-
-# Execute query and check number of rows returned
-res <- dbGetQuery(con, sql)
-if (nrow(res) == 1) {
-  # Successful login, grant access to resources
-  print("Login successful!")
-} else {
-  # Failed login, deny access
-  print("Invalid username or password.")
+# Function to prompt user for login credentials
+get_credentials <- function() {
+  username <- readline("Enter your username: ")
+  password <- readline("Enter your password: ")
+  return(list(username=username, password=password))
 }
 
 
-
-
+# Loop until user provides valid credentials or chooses to exit
+while (TRUE) {
+  # Prompt user for login credentials
+  creds <- get_credentials()
+  
+  
+  # Construct SQL query to check credentials against database
+  sql <- paste0("SELECT * FROM Administrator WHERE Admin_user_name = '",
+                creds$username, "' AND Admin_password = '", creds$password, "'")
+                
+                
+  
+  # Execute query and check number of rows returned
+  res <- dbGetQuery(con, sql)
+  if (nrow(res) == 1) {
+    # Successful login, grant access to resources
+    print("Login successful!")
+    break
+  } else {
+    # Failed login, ask user if they want to try again or exit
+    choice <- readline("Invalid username or password. Enter 'r' to try again or 'q' to quit: ")
+    if (choice == "q") {
+      break
+    }
+  }
+}
