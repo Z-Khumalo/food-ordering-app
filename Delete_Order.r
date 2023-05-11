0b0312d4ba096ae3b7cfed40645d8ee2ad1f7070
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-05-11 15:05:58 +0200

    added a new code

--- Delete_Order.r
+++ Delete_Order.r
@@ -5,22 +5,33 @@ library(DBI)
 library(RPostgreSQL)
 source("con.R")
 
+# Function to delete orders
+delete_orders <- function() {
+  order_id <- readline("Enter the order ID to delete: ")
 
+  return(list(order_id = order_id))
+}
+# Loop 
+while (TRUE) {
+  # Prompt admin for order ID to delete
+  orderss <- delete_orders()
 
-# Get the order ID from the user
-order_id <- readline("Enter the order ID to delete: ")
+  # Define the DELETE query
+  delete_query <- paste("DELETE FROM orders WHERE order_id = ", orderss$order_id)
 
-# Define the DELETE query
-delete_query <- paste("DELETE FROM orders WHERE order_id = ", order_id)
+  # Execute the DELETE query
+  result <- dbExecute(con, delete_query)
 
-# Execute the DELETE query
-result <- dbExecute(con, delete_query)
-
-# Check if any rows were affected
-if (result == 1) {
-  # Successful delete, print message
-  print("Order deleted successfully.")
-} else {
-  # No rows affected, print error message
-  print("Error: Order not found.")
+  # Check if any rows were affected
+  if (result == 1) {
+    # Successful delete, print message
+    print("Order deleted successfully.")
+    break
+  } else {
+    # Failed delete, ask admin if they want to try again or exit
+    choice <- readline("Error: Order not found. Enter 'r' to try again or 'q' to quit: ")
+    if (choice == "q") {
+      break
+    }
+  }
 }
