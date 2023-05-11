29eeacf21453358094b5d0f2df9d76a9ad42b8c4
zandile mdiniso <zandile.mdiniso@dadevs.com>
2023-05-11 15:59:12 +0200

    need to add sequence in order id

--- Add_Order.r
+++ Add_Order.r
@@ -1,83 +1,108 @@
-
+# Load the required packages
 library(DBI)
-library(RPostgres)
+library(RPostgreSQL)
 source("con.R")
 
-con <- dbConnect(
-Postgres(), 
-port = '5432',
-user = 'postgres', 
-password ='', 
-dbname <- 'postgres', 
-host <- 'localhost')
 
+
+# Function to display menu items
 displayMenu <- function() {
-    query <- "SELECT Item_id, Item_name, Item_price FROM Menu"
-    menu_items <- dbGetQuery(con,query)
-    cat("Food Menu:\n")
-    print(menu_items)
+  query <- "SELECT Item_id, Item_name, Item_price FROM Menu"
+  menu_items <- dbGetQuery(con, query)
+  cat("Food Menu:\n")
+  print(menu_items)
 }
 
-
-processOrder <- function() {
-    displayMenu()
+# Function to process the order
+processOrder <- function(admin_id) {
+  # Display the menu
+  displayMenu()
 
     order_data <- data.frame(Order_id = integer(),
+                             Item_id = integer(),
+                            Item_name = character(), 
+                            Order_quantity = integer(),                  
                             Order_date = character(),
                             Order_total = character(),
-                            Item_id = integer(),
+
                             # Admin_id = integer(),
                             stringsAsFactors = FALSE)
 
-    repeat{
-        item_id <- as.integer(readline(prompt = "Enter Item from Menu list: "))
+  
+  # Start a loop to allow the user to add items to the order
+  repeat {
+    # Prompt the user to enter the item ID
+    item_id <- readline(prompt = "Enter the Item ID you wish to add to your order (or type 0 to finish): ")
+    
 
-        if (item_id == 0) {
-            break
-        }
-
-        query <- paste0("SELECT Item_id FROM Menu WHERE Item_id = ", item_id)
-        item_exists <- dbGetQuery(con,query)
-
-        if (nrow(item_exists) ==0) {
+    # Check if the user wants to finish the order
+    if (item_id == "0") {
+      break
+    }
+    
+    # Check if the item ID is a valid integer
+    if (!grepl("^\\d+$", item_id)) {
+      cat("Invalid Item ID. Please enter a valid Item ID.\n")
+      next
+    }
+    
+    item_id <- as.integer(item_id)
+    
+    # Check if the item ID exists in the menu
+    query <- paste0("SELECT Item_id, Item_name, Item_price FROM Menu WHERE Item_id = ", item_id)
+   item_exists <- dbGetQuery(con,query)
+    
+ 
+        if (nrow(item_exists) ==0) { 
             cat("Invalid Item ID. Please enter a valid Item ID from the Menu List.\n")
             next
-        }
-
-        quantity <- as.integer(readline(prompt = "Enter the quantity for this item: "))
-            if (is.na(quantity) || quantity <= 0) {
-                cat("Invalid quantity. Please a positive integer.\n")
-                next
-            }
-
-        order_date <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
-
-        query <- paste0("SELECT Item_price FROM Menu WHERE Item_id=", item_id)
-        item_price <- dbGetQuery(con,query)$item_price
-        total_price <- item_price * quantity
-
-        order_data <- rbind(order_data, data.frame(Order_Id = NA,
-                             Order_quantity = as.character(quantity), 
-                             Order_date = as.character(order_date),
-                             Order_total = as.character(total_price),
-                             Item_id = as.integer(item_id)))
-                            #  Admin_id = as.integer(admin_id)))      
-
+    }
+    
+    quantity <- as.integer(readline(prompt = "Enter the quantity for this item: "))
+    if (is.na(quantity) || quantity <= 0) {
+      cat("Invalid quantity. Please enter a positive integer.\n")
+      next
     }
 
-    cat("Order summary:\n")
-    print(order_data)
+    
+    # Get the current date and time
+    order_date <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
+    
+    # Calculate the total price for the item
+        get_price_query <- paste0("SELECT Item_price, Item_name FROM Menu WHERE Item_id=", item_id)
+        item_price <- dbGetQuery(con,get_price_query)$item_price
+        item_name <- dbGetQuery(con,get_price_query)$item_name
+        total_price <- item_price * quantity
+    
+    # Add the item to the order data frame
+        order_data <- rbind(order_data, data.frame(Order_Id = NA,
+                            Order_quantity = as.character(quantity),
+                            Order_total = as.character(total_price),
+                            Order_date = as.character(order_date),
+                             Item_id = as.integer(item_id),
+                             admin_id = NA,
+                             Item_name = as.character((item_name)
+                             )))
 
-    dbWriteTable(con, "Orders", order_data, append = TRUE, row.names = FALSE)
+    
+                # Prompt the user to add another item
+    add_another <- readline(prompt = "Do you want to add another item to your order? (Y/N): ")
+    if (tolower(add_another) != "y") {
+      break
+    }
+
+  }
+    # Print the order data
+    cat("THANK YOU FOR YOUR ORDER!\n Order summary:\n")
+     print(order_data)
+    
+   dbWriteTable(con, "Final_Orders", order_data, append = TRUE, row.names = FALSE)
 
 }
 
 
-    # getAdminId <- function(admin_user) {
-    #     query <- paste0("SELECT Admin_id FROM Administrator WHERE Admin_user_name = '", admin_user, "'")
-    #     admin_id <- dbGetQuery(con, query)$admin_id
-    #     return(admin_id)
-        
-    
+  # Save the order data to the database
+  
+     print(processOrder())
 
-print(processOrder())
+     ##process needs to take the user back the main menu after order has been processed
