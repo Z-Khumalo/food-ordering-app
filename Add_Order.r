006075204eb0ca566573d914e39e98296847f73a
Z-Khumalo <Zweli.Khumalo@dadevs.co.za>
2023-05-15 10:41:04 +0200

    fecth items from table

--- Add_Order.r
+++ Add_Order.r
@@ -4,7 +4,7 @@ library(RPostgres)
 #input order variable
 ITEMS  = readline()
 
-#Order data frame
+#Order data frames
 order_d = data.frame(
     id = c(),
     title = c(),
@@ -15,3 +15,17 @@ order_tbl <- data.frame(
     order_date <- c(),
     total <- c()
 )
+
+#Connec database
+conn = dbConnect(Postgres(), 
+                user='postgres',
+                port = '5432',
+                password ='', 
+                dbname= 'food_orderingdb', 
+                host='localhost')
+
+#Fetch items from database
+query = ('SELECT id, title, price FROM table87')
+df = dbSendQuery(conn, query)
+df = dbFetch(df)
+menu_lfj= as.list(df$id)
