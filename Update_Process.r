814aa0885c2fdf4ce1891775f2e74a4270a97404
Z-Khumalo <Zweli.Khumalo@dadevs.co.za>
2023-05-17 10:32:24 +0200

    updated script

--- Update_Process.r
+++ Update_Process.r
@@ -6,7 +6,7 @@ source('con.R')
 
 
 #Order data frames
-order_tbl <- data.frame(Qnty = c(), order_date <- c(), total <- c())
+order_tbl <- data.frame()
 
 
 #Fetch items from menu table
@@ -23,6 +23,18 @@ add_order = function(){
        make_order()
     }
 }
+
+remove_item = function(){
+    item_to_remove <- readline(prompt = "Remove: ")
+    item_to_remove <- as.integer(item_to_remove)
+    order_tbl<- order_tbl[-c(item_to_remove),]
+    print(order_tbl)
+}
+
+delete_order = function(){
+    order_tbl = order_tbl[0,]
+    make_order()
+}
 #Function to make order from order table
 make_order <- function(){
     ITEMS <<- readline()
@@ -35,8 +47,8 @@ make_order <- function(){
 
 #Function to fetch items from database
 fetch_orders <- function() {
-    order_tbl1 <-  data.frame(id = c(), title = c(), price = c())
-    query <- paste("SELECT item_id, item_name, item_price, item_category 
+    order_tbl1 <-  data.frame(id = c(), title = c(), price = c(),item_price = c())
+    query <- paste("SELEC T item_id,item_name, item_price, item_category 
                     FROM menu 
                     WHERE item_id =", ITEMS)
     df <- dbSendQuery(conn, query)
@@ -55,8 +67,10 @@ confirm_order <- function(){
     if (order_c == '1'){
         make_order()
     } else if (order_c == '3') {
+        remove_item()
+        add_order()
     } else {
-    print('Thanks for shopping')
+        delete_order()
 }
 }
 
