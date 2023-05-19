4a6f4968389247e824fc2c0d5b183f15fc8360ef
SIBONGILE MOKOENA <132346739+sibongile029@users.noreply.github.com>
2023-05-19 14:54:58 +0200

    Update Delete_Order.r

--- Delete_Order.r
+++ Delete_Order.r
@@ -1,29 +1,34 @@
-
-#Load necessary packages
+# Load necessary packages
 library(DBI)
 library(RPostgreSQL)
 source("con.R")
 
 # Function to delete orders
 delete_orders <- function() {
-order_id_to_delete <- readline(prompt = "Enter the Order_id to delete: ")
-
-# Construct the DELETE statement
-delete_query <- paste0("DELETE FROM Orders WHERE Order_id = '", order_id_to_delete, "'")
-
-# Execute the DELETE statement
-dbExecute(con, delete_query)
+  # Get the Order_id to delete from the user
+  order_id_to_delete <- readline(prompt = "Enter the Order_id to delete: ")
+  
+  # Construct the DELETE statement
+  delete_query <- paste0("DELETE FROM final_orders WHERE order_id = '", order_id_to_delete, "'")
+  
+  # Execute the DELETE statement
+  result <- dbExecute(con, delete_query)
+  
   # Check if any rows were affected
-  if (delete_query >= 1) {
+  if (result >= 1) {
     # Successful delete, print message
     print("Order deleted successfully.")
-    break
   } else {
-    # Failed delete, ask admin if they want to try again or exit
+    # Failed delete, ask user if they want to try again or exit
     choice <- readline("Error: Order not found. Enter 'r' to try again or 'q' to quit: ")
     if (choice == "q") {
-      break
+      return()
+    } else if (choice == "r") {
+      # Recursive call to delete_orders function to try again
+      delete_orders()
     }
   }
 }
-print(delete_orders())
+
+# Call the delete_orders function
+delete_orders()
